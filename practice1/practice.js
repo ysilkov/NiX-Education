291415a10573bd7f2f31239ea4710b60b18048ab
ysilkov <ysilkov@ukr.net>
2021-12-28 20:20:18 +0200

    hi

---
+++ practice1/practice.js
@@ -0,0 +1,10 @@
+const camelCase = 0;
+const snake_case = 1;
+const PascalCase = 2;
+alert(camelCase); // alert выводить цыфри в форме всплывающего окна,
+                  //а консоль лог выводить данные в консоле и их выдно, если зайти в консоль браузера
+alert(snake_case);
+alert(PascalCase);
+console.log(camelCase);
+console.log(snake_case);
+console.log(PascalCase);
